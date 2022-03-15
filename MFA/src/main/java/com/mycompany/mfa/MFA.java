3087c75346e445efb244e5d0462a4041e91bccfe
Terry Keyrouz <terrytyk77@gmail.com>
2022-03-15 20:50:21 +0000

    Java Init

---
+++ MFA/src/main/java/com/mycompany/mfa/MFA.java
@@ -0,0 +1,12 @@
+package com.mycompany.mfa;
+
+/**
+ *
+ * @author Terry Keyrouz
+ */
+public class MFA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
