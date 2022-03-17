e5dc4ce7c37f27deccd4d8e030a5c9d6671379f7
Terry Keyrouz <terrytyk77@gmail.com>
2022-03-17 16:50:56 +0000

    Fixing xml

--- MFA/src/main/java/mfa/controllers/LoginService.java
+++ MFA/src/main/java/mfa/controllers/LoginService.java
@@ -8,21 +8,21 @@ public class LoginService {
     private final DataManager dataManager;
     private final String name;
     private final String password;
-    private AuthenticationInterface mfaAuthentication;
+    private AuthenticationInterface authenticationMethod;
 
     public LoginService(String name, String password){
         this.name = name;
         this.password = password;
-        this.mfaAuthentication = null;
+        this.authenticationMethod = null;
         this.dataManager = DataManager.getInstance();
     }
 
     public AuthenticationInterface getAuthenticationMethod(){
-        return this.mfaAuthentication;
+        return this.authenticationMethod;
     }
 
     public void setAuthenticationMethod(AuthenticationInterface mfaAuthentication){
-        this.mfaAuthentication = mfaAuthentication;
+        this.authenticationMethod = mfaAuthentication;
     }
 
     public boolean loginAttempt() throws InterruptedException {
@@ -30,7 +30,7 @@ public class LoginService {
         TimeUnit.SECONDS.sleep(2);
         if(this.dataManager.getUser(this.name)){
             if(this.dataManager.confirmUserPassword(this.name, this.password)) {
-                if(this.mfaAuthentication.authenticate()) {
+                if(this.authenticationMethod.authenticate()) {
                     Utility.println(this.name + " Logged in Successfully!");
                     return true;
                 }
